9085e3e230e89a1a2fcd40849b3289e9e6e0858c
DincProductions <100015093+DincProductions@users.noreply.github.com>
2022-02-22 14:55:53 +0300

    Add files via upload

---
+++ JS Calculator Functs.js
@@ -0,0 +1,49 @@
+<script>
+//*Javascript Part*//
+function DegerataM() {
+let x,y;
+x=document.getElementById("Xvalue").value;
+y=document.getElementById("Yvalue").value;
+z= x**y ;
+//*There are a lot of functions so we don't doing anything if you don't know*//
+
+ document.getElementById("demo").innerHTML = "The value of z is "  + z + "." ;
+}
+function Degerata1() {
+let x,y;
+x=document.getElementById("Xvalue").value;
+y=document.getElementById("Yvalue").value;
+z= Number(x) + Number(y);
+
+
+ document.getElementById("demo").innerHTML = "The value of z is "  + z + "." ;
+}
+function Degerata2() {
+let x,y;
+x=document.getElementById("Xvalue").value;
+y=document.getElementById("Yvalue").value;
+z= x - y;
+
+
+ document.getElementById("demo").innerHTML = "The value of z is "  + z + "." ;
+}
+function Degerata3() {
+let x,y;
+x=document.getElementById("Xvalue").value;
+y=document.getElementById("Yvalue").value;
+z= x * y;
+
+
+ document.getElementById("demo").innerHTML = "The value of z is "  + z + "." ;
+}
+function Degerata4() {
+let x,y;
+x=document.getElementById("Xvalue").value;
+y=document.getElementById("Yvalue").value;
+z= x / y;
+
+
+ document.getElementById("demo").innerHTML = "The value of z is "  + z + "." ;
+}
+//*This project made by Dinç Erdem Bayraktar*//
+</script>
